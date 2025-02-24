e02e3ac2def78c7c4df4a99b0baada0744d17417
kovalkl <anastasiiakovalklkl@gmail.com>
2025-02-24 16:35:47 +0300

    feat: add isCompleted

--- src/components/TodoApp/TodoApp.tsx
+++ src/components/TodoApp/TodoApp.tsx
@@ -1,13 +1,19 @@
+import { useState } from "react";
+
 import { Stack } from "@mui/material";
 
 import { TodoInput } from "@/components/TodoApp/components/TodoInput/TodoInput";
-import { useState } from "react";
+
+type TodoType = {
+  text: string;
+  isCompleted: boolean;
+};
 
 export const TodoApp = () => {
-  const [todos, setTodos] = useState<string[]>([]);
+  const [todos, setTodos] = useState<TodoType[]>([]);
 
   const addTodo = (newTodo: string) => {
-    setTodos((todos) => [...todos, newTodo]);
+    setTodos((todos) => [...todos, { text: newTodo, isCompleted: false }]);
   };
 
   return (
